cd232b60caab555e71ecf3f2d4b5a4f8e34b48d1
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-28 15:49:26 +0530

    added origin in cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URI, "http://localhost:5000"],
+    origin: [process.env.FRONTEND_URI, "http://localhost:5000","http://localhost:3000"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
