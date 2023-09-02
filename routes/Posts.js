465c86b8cd0ed742bda9071118f93c41223116a8
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-02 09:59:41 +0530

    modified posts routes

--- routes/Posts.js
+++ routes/Posts.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import { isAuthenticated } from "../middlewares/auth.js";
 import { uploadImageToPosts } from "../utils/aws_bucket_services.js";
+import {uploadPost,deletePost,updatePost,getAllPosts} from "../controllers/Posts.js"
 
 router.post(
   "/upload",
