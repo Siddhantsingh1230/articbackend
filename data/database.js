deeb89a0a5c0ba77021c666e90e15510287b5d7e
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-02 10:08:17 +0530

    mutated server.js

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "../app.js";
 
-export const connectDB = () => {
+export const connectDBAndListen = () => {
   mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
