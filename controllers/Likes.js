316eca65eeb806f5cc9bcda4b09994c177ce137d
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-05 22:13:21 +0530

    added likes route

--- controllers/Likes.js
+++ controllers/Likes.js
@@ -3,7 +3,7 @@ import { likesModel } from "../models/Likes.js";
 export const isLiked = async (req, res) => {
   //   const { _id } = req.user;
   const { postID, userID } = req.body;
-  const like = await likesModel.findOne({userID,postID});
+  const like = await likesModel.find({userID,postID});
   if(!like){
     return res.status(200).json({ success: true, message: false });
   }
