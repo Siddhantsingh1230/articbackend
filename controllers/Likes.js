42e14efe002cb685c8fdf651a67bef735a05b6b2
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-05 22:21:37 +0530

    modifiedd likes route

--- controllers/Likes.js
+++ controllers/Likes.js
@@ -2,11 +2,15 @@ import { likesModel } from "../models/Likes.js";
 
 export const isLiked = async (req, res) => {
   //   const { _id } = req.user;
-  const { postID, userID } = req.body;
-  const like = await likesModel.find({userID,postID});
-  if(!like){
-    return res.status(200).json({ success: true, message: false });
+  try {
+    const { postID, userID } = req.body;
+    const like = await likesModel.find({ userID, postID });
+    if (!like) {
+      return res.status(200).json({ success: true, message: false });
+    }
+
+    res.status(200).json({ success: true, message: true });
+  } catch (error) {
+    return res.status(404).json({ success: false, message: error });
   }
-  
-  res.status(200).json({ success: true, message: true });
 };
