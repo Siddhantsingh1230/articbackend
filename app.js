d35e7744517e8a0c2df8a45e2a8f724280dc0e3a
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-09 11:18:11 +0530

    added forgot pwd and resetpwd pages

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ export const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(path.resolve(), "public")));
 app.set("view engine", "ejs");
 app.use(
